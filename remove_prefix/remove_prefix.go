392429869e2913f7b549f51795413e6e3a0f04ac
Benjamin Borbe <bborbe@rocketnews.de>
2016-08-30 10:08:31 +0200

    update http_request

--- remove_prefix/remove_prefix.go
+++ remove_prefix/remove_prefix.go
@@ -3,26 +3,28 @@ package remove_prefix
 import (
 	"net/http"
 	"strings"
+	"github.com/mesos/mesos-go/examples/Godeps/_workspace/src/github.com/golang/glog"
 )
 
 type handler struct {
-	prefix  string
-	handler http.HandlerFunc
+	prefix     string
+	subhandler http.HandlerFunc
 }
 
 func New(prefix string, subhandler http.HandlerFunc) *handler {
 	h := new(handler)
 	h.prefix = prefix
-	h.handler = subhandler
+	h.subhandler = subhandler
 	return h
 }
 
 func (h *handler) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
+	glog.V(2).Infof("remove prefix %v from request", h.prefix)
 	if strings.HasPrefix(request.RequestURI, h.prefix) {
 		request.RequestURI = request.RequestURI[len(h.prefix):]
 	}
 	if strings.HasPrefix(request.URL.Path, h.prefix) {
 		request.URL.Path = request.URL.Path[len(h.prefix):]
 	}
-	h.handler(responseWriter, request)
+	h.subhandler(responseWriter, request)
 }

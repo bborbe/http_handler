f20318bb270bbb1371ef3d6ef1b61b2d059c17f3
Benjamin Borbe <bborbe@rocketnews.de>
2016-08-31 20:59:56 +0200

    only remove prefix if defined

--- remove_prefix/remove_prefix.go
+++ remove_prefix/remove_prefix.go
@@ -1,9 +1,9 @@
 package remove_prefix
 
 import (
+	"github.com/golang/glog"
 	"net/http"
 	"strings"
-	"github.com/golang/glog"
 )
 
 type handler struct {
@@ -19,12 +19,14 @@ func New(prefix string, subhandler http.HandlerFunc) *handler {
 }
 
 func (h *handler) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
-	glog.V(2).Infof("remove prefix %v from request", h.prefix)
-	if strings.HasPrefix(request.RequestURI, h.prefix) {
-		request.RequestURI = request.RequestURI[len(h.prefix):]
-	}
-	if strings.HasPrefix(request.URL.Path, h.prefix) {
-		request.URL.Path = request.URL.Path[len(h.prefix):]
+	if len(h.prefix) > 0 {
+		glog.V(2).Infof("remove prefix '%v' from request", h.prefix)
+		if strings.HasPrefix(request.RequestURI, h.prefix) {
+			request.RequestURI = request.RequestURI[len(h.prefix):]
+		}
+		if strings.HasPrefix(request.URL.Path, h.prefix) {
+			request.URL.Path = request.URL.Path[len(h.prefix):]
+		}
 	}
 	h.subhandler(responseWriter, request)
 }

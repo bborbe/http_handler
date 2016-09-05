5397a49233644182d479648b8a59cb74646144c5
Benjamin Borbe <bborbe@rocketnews.de>
2016-09-05 21:40:29 +0200

    remove javascript

--- auth_html/auth_html.go
+++ auth_html/auth_html.go
@@ -1,77 +1,113 @@
 package auth_html
 
 import (
-	"fmt"
 	"net/http"
 
 	"html/template"
 
 	"github.com/bborbe/http/header"
 	"github.com/golang/glog"
+	"time"
 )
+
+const (
+	fieldNameLogin    = "login"
+	fieldNamePassword = "password"
+	cookieName        = "auth-http-proxy-token"
+)
+
+var expiration = time.Now().Add(24 * time.Hour)
 
 type Check func(username string, password string) (bool, error)
 
 type handler struct {
-	handler    http.HandlerFunc
-	check      Check
-	cookieName string
+	handler http.HandlerFunc
+	check   Check
 }
 
 func New(subhandler http.HandlerFunc, check Check) *handler {
 	h := new(handler)
 	h.handler = subhandler
 	h.check = check
-	h.cookieName = "auth_html_login"
 	return h
 }
 
 func (h *handler) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
 	glog.V(2).Infof("check html auth")
 	if err := h.serveHTTP(responseWriter, request); err != nil {
-		if err := h.printForm(responseWriter); err != nil {
-			glog.Warningf("print login form failed: %v", err)
-			responseWriter.WriteHeader(http.StatusInternalServerError)
-		}
+		glog.Warningf("check html auth failed: %v", err)
+		responseWriter.WriteHeader(http.StatusInternalServerError)
 	}
 }
 
-func (h *handler) printForm(responseWriter http.ResponseWriter) error {
+func (h *handler) serveHTTP(responseWriter http.ResponseWriter, request *http.Request) error {
+	glog.V(2).Infof("check html auth")
+	valid, err := h.validateLoginCookie(request)
+	if err != nil {
+		glog.V(2).Infof("validate login failed: %v", err)
+		return err
+	}
+	if valid {
+		glog.V(2).Infof("login is valid, forward request")
+		h.handler(responseWriter, request)
+	}
+	return h.validateLoginParams(responseWriter, request)
+}
+
+func (h *handler) validateLoginParams(responseWriter http.ResponseWriter, request *http.Request) error {
+	glog.V(2).Infof("validate login via params")
+	login := request.FormValue(fieldNameLogin)
+	password := request.FormValue(fieldNamePassword)
+	valid, err := h.check(login, password)
+	if err != nil {
+		glog.V(2).Infof("check login failed: %v", err)
+		return err
+	}
+	if !valid {
+		glog.V(2).Infof("login failed, show login form")
+		return h.loginForm(responseWriter)
+	}
+	glog.V(2).Infof("login success, set cookie")
+	http.SetCookie(responseWriter, &http.Cookie{
+		Name:    cookieName,
+		Value:   header.CreateAuthorizationToken(login, password),
+		Expires: expiration,
+		Path:    "/",
+		Domain:  request.URL.Host,
+	},
+	)
+	http.Redirect(responseWriter, request, "/", http.StatusTemporaryRedirect)
+	return nil
+}
+
+func (h *handler) validateLoginCookie(request *http.Request) (bool, error) {
+	glog.V(2).Infof("validate login via cookie")
+	cookie, err := request.Cookie(cookieName)
+	if err != nil {
+		glog.V(2).Infof("get cookie %v failed: %v", cookieName, err)
+		return false, nil
+	}
+	user, pass, err := header.ParseAuthorizationToken(cookie.Value)
+	if err != nil {
+		glog.V(2).Infof("parse header failed: %v", err)
+		return false, nil
+	}
+	return h.check(user, pass)
+}
+
+func (h *handler) loginForm(responseWriter http.ResponseWriter) error {
+	glog.V(2).Infof("login form")
 	responseWriter.WriteHeader(http.StatusUnauthorized)
 	var t = template.Must(template.New("loginForm").Parse(HTML))
 	data := struct {
 		CookieName string
 		Title      string
 	}{
-		CookieName: h.cookieName,
+		CookieName: cookieName,
 		Title:      "Login",
 	}
+	responseWriter.Header().Add("Content-Type", "text/html")
 	return t.Execute(responseWriter, data)
-}
-
-func (h *handler) serveHTTP(responseWriter http.ResponseWriter, request *http.Request) error {
-	glog.V(2).Infof("check html auth")
-	cookie, err := request.Cookie(h.cookieName)
-	if err != nil {
-		glog.V(2).Infof("get cookie %v failed: %v", h.cookieName, err)
-		return err
-	}
-	user, pass, err := header.ParseAuthorizationToken(cookie.Value)
-	if err != nil {
-		glog.Warningf("parse header failed: %v", err)
-		return err
-	}
-	result, err := h.check(user, pass)
-	if err != nil {
-		glog.Warningf("check auth for user %v failed: %v", user, err)
-		return err
-	}
-	if !result {
-		glog.V(1).Infof("auth invalid for user %v", user)
-		return fmt.Errorf("auth invalid for user %v", user)
-	}
-	h.handler(responseWriter, request)
-	return nil
 }
 
 const HTML = `<!DOCTYPE html>
@@ -81,7 +117,8 @@ const HTML = `<!DOCTYPE html>
 <meta http-equiv="Content-Language" content="en">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="author" content="Benjamin Borbe">
-<meta name="description" content="Booking App">
+<meta name="description" content="Login Form">
+<link rel="icon" href="data:;base64,=">
 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
 <style>
@@ -89,17 +126,10 @@ html {
 	position: relative;
 	min-height: 100%;
 }
-
 body {
 	margin-top: 60px;
 }
 </style>
-<script>
-function login() {
-	var value = btoa(document.getElementById("login").value+":"+document.getElementById("password").value);
-	document.cookie = "{{.CookieName}}=" + value + "; path=/";
-	document.location.reload();
-}
 </script>
 </head>
 <body>
@@ -107,7 +137,7 @@ function login() {
 	<div class="container">
 		<div class="starter-template">
 
-			<form name="loginForm" class="form-horizontal" action="javascript:login()">
+			<form name="loginForm" class="form-horizontal" action="" method="post">
 				<fieldset>
 
 					<legend>Login required</legend>

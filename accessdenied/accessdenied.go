ecdbdfd8e55d56b81b1027e62eb290a9bcdcae84
Benjamin Borbe <bborbe@rocketnews.de>
2016-11-08 13:53:42 +0100

    move accessdenied

---
+++ accessdenied/accessdenied.go
@@ -0,0 +1,27 @@
+package accessdenied
+
+import (
+	"net/http"
+
+	"fmt"
+
+	"github.com/golang/glog"
+)
+
+type handler struct {
+	message string
+	status  int
+}
+
+// New accessdenied handler
+func New() *handler {
+	h := new(handler)
+	h.status = http.StatusForbidden
+	h.message = http.StatusText(h.status)
+	return h
+}
+
+func (h *handler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	glog.V(2).Infof("%d => %s", h.status, h.message)
+	http.Error(resp, fmt.Sprintf(h.message), h.status)
+}

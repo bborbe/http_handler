e996bb22166777faeb2f6acd95ccfc21bd8fc956
Benjamin Borbe <bborbe@rocketnews.de>
2016-11-09 12:24:44 +0100

    rename and descrease loglevel

--- check/http_handler_check.go
+++ check/http_handler_check.go
@@ -24,9 +24,9 @@ func (h *handler) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 	if err := h.check(); err != nil {
 		glog.V(2).Infof("check => failed: %v", err)
 		http.Error(resp, fmt.Sprintf("check failed"), http.StatusInternalServerError)
-	} else {
-		glog.V(2).Infof("check => ok")
-		resp.WriteHeader(http.StatusOK)
-		fmt.Fprintf(resp, "ok")
+		return
 	}
+	glog.V(2).Infof("check => ok")
+	resp.WriteHeader(http.StatusOK)
+	fmt.Fprintf(resp, "ok")
 }
